136766356b19ddea0a4ed9f415fc61c309c41c23
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2024-05-03 18:35:03 +0300

    d

--- testdrone/main.py
+++ testdrone/main.py
@@ -3,7 +3,7 @@ import time
 # import hashlib
 # hashlib.md5('asdasfasfas')
 
-print('Hello, чччччччччччччччччччччччч32чч')
+print('Hello, чччччччччччччччччччч чччч32чч')
 
 
 # time.sleep(random.randint(1, 5))
