85538a8a8383551e38afd1a87e4eb5922ca25ef9
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2024-05-03 18:27:28 +0300

    добавлен скрипт автосборки

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Переменные
+REPO_URL="https://github.com/darkClaw921/testDrone.git"
+APP_NAME="your_app"
+CONTAINER_NAME="your_container"
+BRANCH="master" # Ветка, которая будет использоваться
+PORT=8080 # Порт, на котором будет работать приложение
+
+# Клонирование репозитория
+git pull -b $BRANCH $REPO_URL
+# cd your_repository
+
+# Сборка Docker-контейнера
+docker build -t $APP_NAME .
+
+# Остановка и удаление старого контейнера (если он существует)
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+# Запуск нового контейнера
+docker run -d -p $PORT:$PORT --name $CONTAINER_NAME $APP_NAME
---
+++ flaskDeploy.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import subprocess
+import os
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # Проверяем, что запрос пришел от GitHub и событие - push
+    if request.headers.get('X-GitHub-Event') == 'push':
+        payload = request.json
+        ref = payload['ref']
+
+        # Проверяем, что событие - push в ветку master
+        if ref == 'refs/heads/master':
+            # Запускаем скрипт для обработки события
+            cwd = '/testdrone'  # Укажите путь к вашей папке
+
+            subprocess.Popen(['deploy.sh'], cwd=cwd)
+
+    return jsonify({'message': 'Webhook received'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
